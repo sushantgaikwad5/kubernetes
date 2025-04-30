c6d47851219cc2d8f939a39d80bfdc348d7ae941
root <root@Linux64.myguest.virtualbox.org>
2025-04-30 19:28:25 +0530

    cluster creation file config yml

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # For AMD64 / x86_64
 
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64
 chmod +x ./kind
 sudo cp ./kind /usr/local/bin/kind
 
